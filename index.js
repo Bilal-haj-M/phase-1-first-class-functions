fed7312db134ea8dec5f8160cb2dc40d093da708
Bilal-hajM <bblal1003@gmail.com>
2023-04-09 22:25:16 +0300

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(back) {
+    back()
+} 
+
+function returnsANamedFunction(){
+    return function name(){}
+    console.log("")
+}
+
+function  returnsAnAnonymousFunction() {
+    return function(){}
+}
+   
+
+   
